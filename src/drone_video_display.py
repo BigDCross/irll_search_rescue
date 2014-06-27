407e50585a44e740ca69df514705b7f5a62108fc
lorinv <lt_vandegrift@live.com>
2014-06-26 21:41:55 -0700

    Added Ardrone Part of the Project

---
+++ src/drone_video_display.py
@@ -0,0 +1,224 @@
+#!/usr/bin/env python
+
+# A basic video display window for the tutorial "Up and flying with the AR.Drone and ROS | Getting Started"
+# https://github.com/mikehamer/ardrone_tutorials_getting_started
+
+# This display window listens to the drone's video feeds and updates the display at regular intervals
+# It also tracks the drone's status and any connection problems, displaying them in the window's status bar
+# By default it includes no control functionality. The class can be extended to implement key or mouse listeners if required
+
+# Import the ROS libraries, and load the manifest file which through <depend package=... /> will give us access to the project dependencies
+import roslib; roslib.load_manifest('irll_search_rescue')
+import rospy
+
+# Import the two types of messages we're interested in
+from sensor_msgs.msg import Image    	 # for receiving the video feed
+from ardrone_autonomy.msg import Navdata # for receiving navdata feedback
+
+# We need to use resource locking to handle synchronization between GUI thread and ROS topic callbacks
+from threading import Lock
+
+# An enumeration of Drone Statuses
+from drone_status import DroneStatus
+
+# The GUI libraries
+'''from PySide import QtCore, QtGui'''
+
+# We need an image convereted to use opencv
+# GABE
+import cv2.cv as cv
+import cv2 as cv2
+from image_converter import ToOpenCV, ToRos
+import numpy as np
+from std_msgs.msg import Int32, Float32, Bool
+
+# Some Constants
+'''CONNECTION_CHECK_PERIOD = 250 #ms
+GUI_UPDATE_PERIOD = 20 #ms'''
+CONNECTION_CHECK_PERIOD = 2.250
+GUI_UPDATE_PERIOD = 0.20
+DETECT_RADIUS = 4 # the radius of the circle drawn when a tag is detected
+
+
+'''class DroneVideoDisplay(QtGui.QMainWindow):'''
+class DroneVideoDisplay():
+	StatusMessages = {
+		DroneStatus.Emergency : 'Emergency',
+		DroneStatus.Inited    : 'Initialized',
+		DroneStatus.Landed    : 'Landed',
+		DroneStatus.Flying    : 'Flying',
+		DroneStatus.Hovering  : 'Hovering',
+		DroneStatus.Test      : 'Test (?)',
+		DroneStatus.TakingOff : 'Taking Off',
+		DroneStatus.GotoHover : 'Going to Hover Mode',
+		DroneStatus.Landing   : 'Landing',
+		DroneStatus.Looping   : 'Looping (?)'
+		}
+	DisconnectedMessage = 'Disconnected'
+	UnknownMessage = 'Unknown Status'
+	
+	def __init__(self):
+		# Construct the parent class
+		'''super(DroneVideoDisplay, self).__init__()'''
+
+		# Setup our very basic GUI - a label which fills the whole window and holds our image
+		'''self.setWindowTitle('AR.Drone Video Feed')
+		self.imageBox = QtGui.QLabel(self)
+		self.setCentralWidget(self.imageBox)'''
+
+		# Subscribe to the /ardrone/navdata topic, of message type navdata, and call self.ReceiveNavdata when a message is received
+		self.subNavdata = rospy.Subscriber('/ardrone/navdata',Navdata,self.ReceiveNavdata) 
+		
+		# Subscribe to the drone's video feed, calling self.ReceiveImage when a new frame is received
+		self.subVideo   = rospy.Subscriber('/ardrone/image_raw',Image,self.ReceiveImage)
+		
+		# Holds the image frame received from the drone and later processed by the GUI
+		self.image = None
+		self.imageLock = Lock()
+		# GABE
+                cv.NamedWindow("windowimage", cv.CV_WINDOW_AUTOSIZE)
+
+		self.tags = []
+		self.tagLock = Lock()
+		
+		# Holds the status message to be displayed on the next GUI update
+		self.statusMessage = ''
+
+		# Tracks whether we have received data since the last connection check
+		# This works because data comes in at 50Hz but we're checking for a connection at 4Hz
+		self.communicationSinceTimer = False
+		self.connected = False
+
+		# A timer to check whether we're still connected
+		'''self.connectionTimer = QtCore.QTimer(self)
+		self.connectionTimer.timeout.connect(self.ConnectionCallback)
+		self.connectionTimer.start(CONNECTION_CHECK_PERIOD)'''
+                # GABE
+                self.connectionTimer = rospy.Timer(rospy.Duration(CONNECTION_CHECK_PERIOD), self.ConnectionCallback)
+		
+		# A timer to redraw the GUI
+		'''self.redrawTimer = QtCore.QTimer(self)
+		self.redrawTimer.timeout.connect(self.RedrawCallback)
+		self.redrawTimer.start(GUI_UPDATE_PERIOD)'''
+                self.redrawTimer = rospy.Timer(rospy.Duration(GUI_UPDATE_PERIOD), self.RedrawCallback)
+
+	# Called every CONNECTION_CHECK_PERIOD ms, if we haven't received anything since the last callback, will assume we are having network troubles and display a message in the status bar
+	def ConnectionCallback(self, event):
+		self.connected = self.communicationSinceTimer
+		self.communicationSinceTimer = False
+
+	def RedrawCallback(self, event):
+		if self.image is not None:
+			# We have some issues with locking between the display thread and the ros messaging thread due to the size of the image, so we need to lock the resources
+			self.imageLock.acquire()
+			try:			
+					# Convert the ROS image into a QImage which we can display
+					'''image = QtGui.QPixmap.fromImage(QtGui.QImage(self.image.data, self.image.width, self.image.height, QtGui.QImage.Format_RGB888))'''
+                                        image_cv = ToOpenCV(self.image)
+					'''if len(self.tags) > 0:
+						self.tagLock.acquire()
+						try:
+							painter = QtGui.QPainter()
+							painter.begin(image)
+							painter.setPen(QtGui.QColor(0,255,0))
+							painter.setBrush(QtGui.QColor(0,255,0))
+							for (x,y,d) in self.tags:
+								r = QtCore.QRectF((x*image.width())/1000-DETECT_RADIUS,(y*image.height())/1000-DETECT_RADIUS,DETECT_RADIUS*2,DETECT_RADIUS*2)
+								painter.drawEllipse(r)
+								painter.drawText((x*image.width())/1000+DETECT_RADIUS,(y*image.height())/1000-DETECT_RADIUS,str(d/100)[0:4]+'m')
+							painter.end()
+						finally:
+							self.tagLock.release()'''
+			finally:
+				self.imageLock.release()
+
+                        # GABE
+                        # Convert BGR to HSV
+                        frame = np.asarray(image_cv)
+						
+
+			'''
+                        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+			# ADJUST these values to get a greater range of color
+                        lower_gray = np.array([ 8, 6, 71], dtype=np.uint8)
+                        upper_gray = np.array([50,45,135], dtype=np.uint8)
+
+			lower_white = np.array([ 0, 0,245], dtype=np.uint8)
+                        upper_white = np.array([10,10,255], dtype=np.uint8)
+                        # Threshold the HSV image to get only white color
+                        goal_mask = cv2.inRange(hsv, lower_white, upper_white)
+
+                        # Threshold the HSV image to get only gray colors
+                        mask = cv2.inRange(hsv, lower_gray, upper_gray)
+                        flag, mask = cv2.threshold(mask, 100, 255, cv2.THRESH_BINARY)
+                        flag, goal_mask = cv2.threshold(goal_mask, 100, 255, cv2.THRESH_BINARY)                        
+ 
+                        # NEEDS TO PUBLISH Percentage, size, number of gray
+                        number = cv2.countNonZero(mask)
+                        size = mask.shape[0] * mask.shape[1]
+                        percentage = (number * 1.0) / (size * 1.0)
+			pubSize = rospy.Publisher('/ardrone_learning/SizeGray', Int32)
+                        pubPercent = rospy.Publisher('/ardrone_learning/PercentGray', Float32)        
+                        pubSize.publish(data=number)
+			pubPercent.publish(data=percentage)
+
+			goal_number = cv2.countNonZero(goal_mask)
+                        goal_percentage = (goal_number * 1.0) / (size * 1.0)
+
+			#rospy.loginfo("Gray Percentage: %s", str(percentage))
+                        pubGoal = rospy.Publisher('/ardrone_learning/IsGoal', Bool)
+			if goal_percentage >= 45.0:
+				pubGoal.publish(data=True)
+			else:
+				pubGoal.publish(data=False)                       
+ 
+                        cv2.imshow("goal_mask", goal_mask)
+                        cv2.imshow("mask", mask)
+                        cv.ShowImage("windowimage", image_cv)
+                        cv.WaitKey(2)
+			'''
+			# We could  do more processing (eg OpenCV) here if we wanted to, but for now lets just display the window.
+			'''self.resize(image.width(),image.height())
+			self.imageBox.setPixmap(image)'''
+
+		# Update the status bar to show the current drone status & battery level
+		'''self.statusBar().showMessage(self.statusMessage if self.connected else self.DisconnectedMessage)'''
+
+	def ReceiveImage(self,data):
+		# Indicate that new data has been received (thus we are connected)
+		self.communicationSinceTimer = True
+
+		# We have some issues with locking between the GUI update thread and the ROS messaging thread due to the size of the image, so we need to lock the resources
+		self.imageLock.acquire()
+		try:
+			self.image = data # Save the ros image for processing by the display thread
+		finally:
+			self.imageLock.release()
+
+	def ReceiveNavdata(self,navdata):
+		# Indicate that new data has been received (thus we are connected)
+		self.communicationSinceTimer = True
+
+		# Update the message to be displayed
+		'''msg = self.StatusMessages[navdata.state] if navdata.state in self.StatusMessages else self.UnknownMessage
+		self.statusMessage = '{} (Battery: {}%)'.format(msg,int(navdata.batteryPercent))
+
+		self.tagLock.acquire()
+		try:
+			if navdata.tags_count > 0:
+				self.tags = [(navdata.tags_xc[i],navdata.tags_yc[i],navdata.tags_distance[i]) for i in range(0,navdata.tags_count)]
+			else:
+				self.tags = []
+		finally:
+			self.tagLock.release()'''
+
+if __name__=='__main__':
+	import sys
+	rospy.init_node('ardrone_video_display')
+	'''app = QtGui.QApplication(sys.argv)'''
+	display = DroneVideoDisplay()
+	'''display.show()'''
+	'''status = app.exec_()'''
+	rospy.signal_shutdown('Great Flying!')
+	sys.exit(status)
---
+++ src/keyboard_controller.py
@@ -0,0 +1,580 @@
+#!/usr/bin/env python
+
+# The Keyboard Controller Node for the tutorial "Up and flying with the AR.Drone and ROS | Getting Started"
+# https://github.com/mikehamer/ardrone_tutorials
+
+# This controller extends the base DroneVideoDisplay class, adding a keypress handler to enable keyboard control of the drone
+
+# Import the ROS libraries, and load the manifest file which through <depend package=... /> will give us access to the project dependencies
+import roslib; roslib.load_manifest('irll_search_rescue')
+import rospy
+import os
+import errno
+import argparse
+import random
+
+# Load the DroneController class, which handles interactions with the drone, and the DroneVideoDisplay class, which handles video display
+from drone_controller import BasicDroneController
+from drone_video_display import DroneVideoDisplay
+
+# Finally the GUI libraries
+'''from PySide import QtCore, QtGui'''
+
+# GABE
+import cv2.cv as cv
+import time
+
+import geometry_msgs.msg
+from gazebo_msgs.msg import ModelStates
+from gazebo_msgs.msg import ModelState
+from std_msgs.msg import String
+from std_msgs.msg import Float32 
+from std_msgs.msg import Bool
+
+class State:
+   def __init__(self):
+      self.x = 0
+      self.y = 0
+      self.actions = {'forward': 0, 'left': 0, 'right': 0,
+          'turn_right': 0, 'turn_left': 0}
+      self.special = None
+
+class ARDroneController():
+  def __init__(self):
+    self.pitch = 0.0
+    self.roll = 0.0
+    self.yaw_velocity = 0.0
+    self.z_velocity = 0.0
+    self.controller = BasicDroneController()
+    self.UpdateDroneState()
+    
+    #Lorin
+    self.found = False
+    self.init_copter_x_position = 999
+    self.init_copter_y_position = 999
+
+    self.subPoseData = rospy.Subscriber('/gazebo/model_states', ModelStates, 
+        self.ReceiveModelStates)
+
+  def Takeoff(self):
+    self.controller.SendTakeoff()
+    time.sleep(1)
+
+  def Land(self):
+    self.controller.SendLand()
+    time.sleep(1)
+
+  def Emergency(self):
+    self.controller.SendEmergency()
+    time.sleep(1)
+
+  # FIXME convert x to meters
+  def Forward(self, x):
+    while x > 0:
+      self.pitch += 1.5
+      self.UpdateDroneState(pitch=self.pitch)
+      x -= 1
+    #self.pitch -= 1.0
+    #self.UpdateDroneState(pitch=self.pitch)
+
+  # FIXME convert x to meters
+  def Backward(self, x):
+    while x > 0:
+      self.pitch += -1.5
+      self.UpdateDroneState(pitch=self.pitch)
+      x -= 1
+    #self.pitch -= -1.0
+    #self.UpdateDroneState(pitch=self.pitch)
+
+  # FIXME convert x to meters
+  def Left(self, x):
+    while x > 0:
+      self.roll += 1.0
+      self.UpdateDroneState(roll=self.roll)
+      x -= 1
+    #self.roll -= 1.0
+    #self.UpdateDroneState(roll=self.roll)
+
+  # FIXME convert x to meters
+  def Right(self, x):
+    while x > 0:
+      self.roll += -1.0
+      self.UpdateDroneState(roll=self.roll)
+      x -= 1
+    #self.roll -= -1.0
+    #self.UpdateDroneState(roll=self.roll)
+
+  # FIXME convert x to degrees
+  def TurnLeft(self, x):
+    while x > 0:
+      self.yaw_velocity += 1.5
+      self.UpdateDroneState(yaw=self.yaw_velocity)
+      x -= 1
+    self.yaw_velocity -= 1.0
+    self.UpdateDroneState(yaw=self.yaw_velocity)
+
+  # FIXME convert x to degrees
+  def TurnRight(self, x):
+    while x > 0:
+      self.yaw_velocity += -1.5
+      self.UpdateDroneState(yaw=self.yaw_velocity)
+      x -= 1
+    self.yaw_velocity -= -1.0
+    self.UpdateDroneState(yaw=self.yaw_velocity)
+
+  # FIXME convert x to meters
+  def Up(self, x):
+    while x > 0:
+      self.z_velocity += 1.5
+      self.UpdateDroneState(altitude=self.z_velocity)
+      x -= 1
+    #self.z_velocity -= 1.0
+    #self.UpdateDroneState(altitude=self.z_velocity)
+
+  # FIXME convert x to meters
+  def Down(self, x):
+    while x > 0:
+      self.z_velocity += -1.5
+      self.UpdateDroneState(altitude=self.z_velocity)
+      x -= 1
+    #self.z_velocity -= -1.0
+    #self.UpdateDroneState(altitude=self.z_velocity)
+
+  def Stop(self):
+    self.UpdateDroneState()
+
+  def UpdateDroneState(self, roll=0, pitch=0, yaw=0, altitude=0):
+    self.roll = roll
+    self.pitch = pitch
+    self.yaw_velocity = yaw
+    self.z_velocity = altitude
+    self.controller.SetCommand(self.roll, self.pitch, self.yaw_velocity, 
+        self.z_velocity)
+    time.sleep(1)
+  def ReturnHome(self):
+    print "Returning Home!"
+
+       #while (self.copter_x_position >= 2) or (self.copter_x_position <= -2):
+    if self.copter_y_position > 2:
+      self.Right(1)
+      time.sleep(1)
+      self.Stop()	  
+      time.sleep(1)
+    elif self.copter_y_position < -2: 
+      self.Left(1)
+      time.sleep(1)	  
+      self.Stop()	  
+      time.sleep(1)
+    else:
+      self.Stop()
+      controller.Land()
+
+  def ReceiveModelStates(self, model_states):
+    if self.init_copter_x_position == 999:
+      self.init_copter_x_position = int(model_states.pose[3].position.x)
+      self.init_copter_y_position = int(model_states.pose[3].position.y)
+      print "Initial State Recorded."
+
+    print "hi!"
+    self.copter_x_position = int(model_states.pose[3].position.x)
+    self.copter_y_position = int(model_states.pose[3].position.y)
+    self.target_x_position = int(model_states.pose[1].position.x)
+    self.target_y_position = int(model_states.pose[1].position.y)
+
+    if (self.copter_x_position <= self.target_x_position + .5 and self.copter_x_position >= self.target_x_position - .5) and (self.copter_y_position <= self.target_y_position + .5 and self.copter_y_position >= self.target_y_position - .5):
+	print "You Have Located the Target!"
+	self.found = True
+	self.ReturnHome()
+	
+		 
+    # Determine state
+    #self.__current_state = (self.x_position, self.y_position)
+
+class Experiment():
+
+  def __init__(self, controller, display):
+    self.display = display
+    self.controller = controller
+
+    reset_model_pub = rospy.Publisher('/gazebo/set_model_state', ModelState)
+
+    self.subPoseData = rospy.Subscriber('/gazebo/model_states', ModelStates, 
+        self.ReceiveModelStates)
+    #rospy.Subscriber('targetSeen', String, self.callback1)
+    #rospy.Subscriber('targetX', String, self.callback2)
+    #rospy.Subscriber('targetY', String, self.callback3)
+    rospy.Subscriber('/ardrone_learning/PercentGray', Float32, self.PercentGray)
+    rospy.Subscriber('/ardrone_learning/IsGoal', Bool, self.IsGoal)
+
+    width = 8
+    height = 20
+
+    self.__width = width
+    self.__height = height
+    self.__current_state = (0,0)
+    self.__default_q = 1
+    self.q_map = q_map
+    #self.__actions = {'forward', 'backward', 'left', 'right', 'turn_right', 
+    #    'turn_left'}
+    self.__actions = {'forward', 'left', 'right', 'turn_right', 'turn_left'}
+    self.world = self.__build_world(width, height)
+    self.__total_reward = 0
+    self.__episode_reward = 0
+    self.__turn = 0
+    self.end_episode = False
+    self.is_goal = False
+    
+
+
+  def PercentGray(self, gray):
+    #rospy.loginfo('gray: {}'.format(gray))
+    if gray.data >= 0.06:
+      # Crashing into a wall end episode
+      reward = -100
+      self.__total_reward -= 100
+      self.__episode_reward -= 100
+      self.end_episode = True
+
+
+  def __build_world(self, width, height):
+    grid = []
+    # Build the rest of the rows
+    for _ in range(height):
+      row = []
+      # Build a single row
+      for _ in range(width):
+        row.append(State())
+      grid.append(row)
+
+    return grid
+
+
+  def __get_q(self, x, y, action):
+    return self.world[x][y].actions[action]
+
+
+  def __set_q(self, x, y, action, reward):
+    self.world[x][y].actions[action] = reward
+
+
+  def __get_max_action(self, x, y):
+
+    for value in self.world[x][y].actions.values():
+      if value != self.__default_q:
+        action = max(self.world[x][y].actions, key=self.world[x][y].actions.get)
+        return action[0]
+
+  """
+  <<<<<<< Updated upstream
+    def ReceiveModelStates(self, model_states):
+      self.x_position = model_states.pose[10].position.x
+      self.y_position = model_states.pose[10].position.y
+      #rospy.loginfo("The values of x & y : %s %s",str(self.x_position), str(self.y_position))
+  =======
+      return 'N'
+  >>>>>>> Stashed changes
+  """
+  def __get_max_action_value(self, x, y):
+
+    for value in self.world[x][y].actions.values():
+      if value != self.__default_q:
+        action = max(self.world[x][y].actions, key=self.world[x][y].actions.get)
+        return self.world[x][y].actions[action]
+
+    return 'N'
+
+  def __choose_action(self, state):
+    # Take the highest Q action
+    # Default to a huge negative number to force it to be reset
+    x, y = state
+    greedy_q_value = -10000000000
+    for action in self.world[x][y].actions:
+      new_q_value = self.__get_q(x, y, action)
+      if  new_q_value > greedy_q_value:
+        greedy_action = action
+        greedy_q_value = new_q_value
+
+    # 10% of the time take an exploratory action instead
+    percent = random.randint(1, 10)
+
+    # Explore action
+    if percent == 1:
+      action = self.__explore(greedy_action)
+    else:
+      action = greedy_action
+
+    return action
+
+
+  def __explore(self, exclude_action):
+    explore_actions = []
+    for action in self.__actions:
+      if action != exclude_action:
+        explore_actions.append(action)
+
+    return random.choice(explore_actions)
+
+
+  def __move(self, turn, action, f):
+    # Set the new state as the current in case none of the actions are valid
+    new_state = self.__current_state
+    old_state = self.__current_state
+    x, y = self.__current_state
+
+    if action == 'forward':
+      self.controller.Forward(1)
+
+    #if action == 'backward':
+    #  self.controller.Backward(1)
+
+    if action == 'left':
+      self.controller.Left(1)
+
+    if action == 'right':
+      self.controller.Right(1)
+
+    if action == 'turn_left':
+      self.controller.TurnLeft(1)
+
+    if action == 'turn_right':
+      self.controller.TurnRight(1)
+
+  def IsGoal(self, is_goal):
+    # If the new state is the goal, hooray! 100 points
+
+    if is_goal.data is True:
+      self.controller.Land()
+      time.sleep(3)
+      # Goal state found!
+      reward = 100
+      self.__total_reward += 100
+      self.__episode_reward += 100
+      self.record_move(self.__turn, x, y, action, reward, f)
+
+      # Record end game values
+      self.record_end_game(f, True)
+      self.is_goal = True
+
+  def get_reward(self, action, f):
+    # Haven't found the goal yet keep looking -1 point
+    x, y = self.__current_state
+    reward = -1
+    self.__total_reward -= 1
+    self.__episode_reward -= 1
+    self.record_move(self.__turn, x, y, action, reward, f)
+
+    return reward
+
+  def episode(self, max_num_turns, q_map, f, alpha, gamma):
+    if q_map is not None:
+      self.world = q_map
+
+    # Choose action from state using policy derived from Q (e.g. e-greedy)
+    action = self.__choose_action(self.__current_state)
+
+    # Repeat (for each step of episode):
+    self.__turn = -1
+
+    self.controller.Takeoff()
+    time.sleep(3)
+    #for turn in range(max_num_turns):
+    while max_num_turns:
+
+      self.__turn += 1
+      # Take action a, observe r, s'
+      old_state = self.__current_state
+      old_action = action
+      self.__move(self.__turn, action, f)
+      # Wait for move command to go into effect
+      #time.sleep(1)
+
+      if self.end_episode is True:
+        reward = -100
+
+        new_state = self.__current_state
+        # Choose action' from state' using policy derived from Q(e.g. e-greedy)
+        new_action = self.__choose_action(new_state)
+
+        qsa = self.__get_q(old_state[0], old_state[1], old_action)
+        qsa_new = self.__get_q(new_state[0], new_state[1], new_action)
+        updated_reward = qsa + (alpha * (reward + gamma * qsa_new - qsa))
+        self.__set_q(old_state[0], old_state[1], old_action, updated_reward)
+        self.controller.Land()
+        time.sleep(3)
+        return self.world
+
+      if self.is_goal is True:
+        return self.world
+
+      reward = self.get_reward(action, f)
+
+      new_state = self.__current_state
+      # Choose action' from state' using policy derived from Q(e.g. e-greedy)
+      new_action = self.__choose_action(new_state)
+
+      # Q(s,a) <- Q(s,a) + alpha[r + gamma * Q(s',a') - Q(s,a)]
+      qsa = self.__get_q(old_state[0], old_state[1], old_action)
+      qsa_new = self.__get_q(new_state[0], new_state[1], new_action)
+      updated_reward = qsa + (alpha * (reward + gamma * qsa_new - qsa))
+      self.__set_q(old_state[0], old_state[1], old_action, updated_reward)
+      # s <- s'; a <- a';
+      action = new_action
+      rospy.loginfo("#: {} Action: {}".format(max_num_turns, action))
+      max_num_turns -= 1
+      time.sleep(1)
+
+    # If we get here, we maxed out the number of turns
+    print("Max turns reached before goal state :(")
+    # Record end game values
+    self.record_end_game(f, False)
+
+
+  def record_move(self, turn, x, y, action, reward, f):
+    move = ', '.join([str(turn), str(x), str(y), action, str(reward)])
+    f.write(move + '\n')
+
+
+  def record_end_game(self, f, goal):
+    f.write("Total Reward: " + str(self.__total_reward) + '\n')
+    f.write("Episode Reward: " + str(self.__episode_reward) + '\n')
+    # Reset the episode reward 
+    self.__episode_reward = 0
+
+    # Print value map of best actions
+    for y in range(self.__height):
+      # Solid line across
+      for _ in range(41):
+        f.write('-')
+      f.write('\n')
+      # Vertical lines and values
+      f.write('| ')
+      for x in range(self.__width):
+        f.write(self.__get_max_action(x,y))
+        f.write(' | ')
+      f.write('\n') 
+
+    # Solid line across
+    for _ in range(41):
+      f.write('-')
+    f.write('\n')
+    f.write('\n')
+
+    self.record_end_game_values (f)
+
+
+  def record_end_game_values(self, f):
+    # Print value map of best actions
+    for y in range(self.__height):
+      # Solid line across
+      for _ in range(41):
+        f.write('-')
+      f.write('\n')
+      # Vertical lines and values
+      f.write('| ')
+      for x in range(self.__width):
+        action_value = self.__get_max_action_value(x,y)
+        if action_value == 'N':
+          action_value = 100
+        f.write('{:.2f}'.format(action_value))
+        f.write(' | ')
+      f.write('\n') 
+
+    # Solid line across
+    for _ in range(41):
+      f.write('-')
+    f.write('\n')
+
+
+  #def episode(self):
+  #  time.sleep(1)
+  #  self.controller.Takeoff()
+  #  self.controller.Forward(1)
+
+  
+# Setup the application
+if __name__=='__main__':
+  import sys
+  # Firstly we setup a ros node, so that we can communicate with the other packages
+  rospy.init_node('ardrone_auto_controller', anonymous=True)
+
+  # Now we construct our Qt Application and associated controllers and windows
+  '''app = QtGui.QApplication(sys.argv)'''
+
+  max_num_turns = 10000000000
+  q_map = None
+  alpha = 0.1
+  gamma = 0.5
+
+  controller = ARDroneController()
+  #experiment = Experiment()
+  display = DroneVideoDisplay()
+
+
+  path = str(3)
+  try:
+    os.makedirs(path)
+  except OSError as exc: 
+    if exc.errno == errno.EEXIST and os.path.isdir(path):
+      pass
+    else: 
+      raise exc
+
+  #for episode in range(episodes):
+  time.sleep(4)
+  controller = ARDroneController()
+  controller.Takeoff()
+  time.sleep(3)
+ 
+  while 1:
+      if not controller.found:	  
+        controller.Forward(1)
+        controller.Forward(1)
+        time.sleep(2)
+        controller.Stop()
+        time.sleep(2)
+
+      if not controller.found:	  
+        controller.Right(1)
+        time.sleep(1)
+        controller.Stop()
+        time.sleep(2)
+
+      if not controller.found:	  
+        controller.Backward(1)
+        controller.Backward(1)
+        time.sleep(2)
+        controller.Stop()
+        time.sleep(2)
+
+      if not controller.found:	  
+        controller.Right(1)
+        time.sleep(1)
+        controller.Stop()
+        time.sleep(2)
+  
+      if controller.found:
+        print "Done"
+        controller.ReturnHome()
+	#break	  
+
+#  rospy.spin()
+
+    #experiment = Experiment(controller, display)
+    #f = open('{}/{}_gridworld_results_{}.txt'.format(3, 3,
+    #    episode), 'w')
+    #q_map = experiment.episode(max_num_turns, q_map, f, alpha, gamma)
+    # Reset the quadcopter
+    #os.system("rosservice call /gazebo/set_model_state '{model_state: "
+    #    "{ model_name: quadrotor, pose: { position: { x: 0, y: 0, z: 0.1 }, "
+    #    "orientation: {x: 0, y: 0, z: 0, w: 1} }, twist: "
+    #    "{linear: {x: 0, y: 0, z: 0 }, angular: {x: 0, y: 0, z: 0} }, "
+    #    "reference_frame: world } }'")
+    #del experiment
+    #del controller
+    #f.close()
+
+	# and only progresses to here once the application has been shutdown
+	#rospy.signal_shutdown('Great Flying!')
+	#sys.exit()
+
+
